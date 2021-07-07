be078447b08a82aa2c199e1dcdc08137dbf48db9
Ale <alearanibar@gmail.com>
2021-07-07 14:17:17 -0700

    Cached posts

--- pages/api/search.js
+++ pages/api/search.js
@@ -7,7 +7,8 @@ export default (req, res) => {
   let posts;
 
   if (process.env.NODE_ENV === "production") {
-    // fetch from cache
+    // fetch from cache in production
+    posts = require("../../cache/data").posts;
   } else {
     const files = fs.readdirSync(path.join("posts"));
     posts = files.map((filename) => {
